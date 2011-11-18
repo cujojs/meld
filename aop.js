e22789178d7d8f22d146a3b0c77b91c6aba1775d
Brian Cavalier <brian@briancavalier.com>
2011-11-18 11:03:19 -0500

    Beginnings of refactor ... aspects are now stored as a unit, and advisor is a more coherent object.  Around advice not implemented yet, and pointcuts/arrays and prototype advising still not even touched

--- aop.js
+++ aop.js
@@ -16,7 +16,7 @@ define([], function() {
 	append  = ap.push;
 	slice   = ap.slice;
 
-	isArray = Array.isArray || function isArrayShim(it) {
+	isArray = Array.isArray || function(it) {
 		return Object.prototype.toString.call(it) == '[object Array]';
 	};
 
@@ -25,30 +25,63 @@ define([], function() {
 		return slice.call(a);
 	}
 
+	function forEach(array, func) {
+        for(var i=0, len=array.length; i<len; ++i) {
+            func(array[i]);
+        }
+	}
+
+	function forEachReverse(array, func) {
+        for(var i=array.length-1; i>=0; --i) {
+            func(array[i]);
+        }
+	}
+
+    var iterators = {
+        // Before uses reverse iteration
+        before: forEachReverse
+    };
+
+    // All other advice types use forward iteration
+    iterators.around
+        = iterators.on
+        = iterators.afterReturning
+        = iterators.afterThrowing
+        = iterators.after
+        = forEach;
+
 	function Advisor(target, func) {
 
 		var orig, advisor;
 
 		this.target = target;
 		this.func = func;
+        this.aspects = [];
 
 		orig = this.orig = target[func];
 		advisor = this;
 
-		target[func] = function() {
+		this.advised = function() {
 			var args, result, afterType, exception;
 
+			function callOrig(args) {
+				var result = orig.apply(target, args);
+				advisor._callAdvice('on', args);
+
+				return result;
+			}
+
 			function callAfter(afterType, args) {
-				advisor._callAdvice(advisor.head, afterType, target, args);
+				advisor._callAdvice(afterType, args);
 			}
 
 			args = argsToArray(arguments);
 			afterType = 'afterReturning';
 
-			advisor._callAdvice(advisor.tail, 'before', target, args);
+			advisor._callAdvice('before', args);
 
 			try {
-				result = orig.apply(target, args);
+				result = callOrig(args);
 			} catch(e) {
 				result = exception = e;
 				afterType = 'afterThrowing'
@@ -59,62 +92,53 @@ define([], function() {
 			callAfter(afterType, args);
 			callAfter('after', args);
 
-			advisor._callAdvice(advisor.head, 'after', target, args);
+			advisor._callAdvice('after', args);
 
 			if(exception) {
 				throw exception;
 			}
 
 			return result;
-		}
+		};
+
+		this.advised._advisor = this;
 	}
 
 	Advisor.prototype = {
 		
 		// Invoke all advice functions in the supplied context, with the
 		// supplied args.
-		_callAdvice: function(list, adviceType, context, args) {
+		_callAdvice: function(adviceType, args) {
 
-			var direction, current, advice;
+			var iterator, context;
+			
+			// before advice runs LIFO, from most-recently added to least-recently added.
+			// All other advice is FIFO
+			iterator = iterators[adviceType];
+			context = this.target;
 
-			direction = list === this.tail ? 'prev' : 'next';
-			current = list;
-
-			while (current) {
-				advice = current.aspect[adviceType];
-				if (advice) {
-					advice.apply(context, args);
-				}
-
-				current = current[direction];
-			}
+            iterator(this.aspects, function(aspect) {
+                var advice = aspect[adviceType];
+                advice && advice.apply(context, args);
+            });
 		},
 
 		// Adds the supplied aspect to the advised target method
 		add: function(aspect) {
+            
+			var aspects, remove;
+			
+			aspects = this.aspects;
+			aspects.push(aspect);
 
-			var aspectItem, remove;
-
-			aspectItem = {
-				prev: this.tail,
-				aspect: aspect
+			remove = function() {
+				for(var i = aspects.length; i >= 0; --i) {
+					if(aspects[i] === aspect) {
+						aspects.slice(i, 1);
+					}
+				}
 			};
 
-			if(this.tail) {
-				this.tail = this.tail.next = aspectItem;
-			} else {
-				this.tail = this.head = aspectItem;
-			}
-
-			remove = aspectItem.remove = function() {
-				var prev, next;
-
-				prev = aspectItem.prev;
-				next = aspectItem.next;
-
-				if(prev) { prev.next = next; }
-				if(next) { next.prev = prev; }
-			};
 
 			return remove;
 		},
@@ -139,14 +163,15 @@ define([], function() {
 
 		advisor = advised._advisor;
 		if(!advisor) {
-			advisor = advised._advisor = new Advisor(target, func);
+			advisor = new Advisor(target, func);
+			target[func] = advisor.advised;
 		}
 
 		return advisor;
 	};
 
-	function addAspect(target, method, aspect) {
-		var advisor = Advisor.get(findTarget(target), method);
+	function addAspectToMethod(target, method, aspect) {
+		var advisor = Advisor.get(target, method);
 
 		if(advisor) {
 			return advisor.add(aspect);
@@ -155,219 +180,42 @@ define([], function() {
 		}
 	}
 
-	// Invoke all advice functions in the supplied context, with the
-	// supplied args.
-	function callAdvice(advices, context, args) {
-		var i, advice;
+	function addAspectToAll(target, methodArray, aspect) {
+		var removers, f, i;
 
+		removers = [];
 		i = 0;
-		
-		while((advice = advices[i++])) {
-			advice.apply(context, args);
+		while((f = methodArray[i++])) {
+			removers.push(addAspectToMethod(target, f, aspect));
 		}
-	}
 
-	// Creates a function to add a new advice function in the correct
-	// order (prepend or append).
-	function makeAdviceAdd(advices, order) {
-		return function(adviceFunc) {
-			if(isArray(adviceFunc)) {
-				for (var i = 0, len = adviceFunc.length; i < len; i++) {
-					order.call(advices, adviceFunc[i]);
-				}
-			} else {
-				order.call(advices, adviceFunc);
-			}
-		};
-	}
-	
-	// Returns the advisor for the target object-function pair.  A new advisor
-	// will be created if one does not already exist.
-	function getAdvisor(target, func) {
-		if(!(func in target)) return;
-
-		var advised = target[func];
-
-		if(typeof advised !== 'function') throw new Error('Advice can only be applied to functions: ' + func);
-		
-		if(!advised._advisor) {
-			var orig, before, on, around, afterReturning, afterThrowing, after;
-
-			// Save the original, not-yet-advised function
-			orig = advised;
-			
-			// Advices.  They'll be invoked in this order.
-			before = [];
-			around = {};
-			on = [];
-			afterReturning = [];
-			afterThrowing  = [];
-			after = [];
-
-			// Intercept calls to the original function, and invoke
-			// all currently registered before, around, and after advices
-			advised = target[func] = function() {
-				var self, targetArgs, result, afterType;
-
-				self = this;
-				targetArgs = argsToArray(arguments);
-				afterType = afterReturning;
-
-				function callAdviceWithContext(advice, targetArgs) {
-					callAdvice(advice, self, targetArgs);
-				}
-
-				// Befores
-				callAdviceWithContext(before, targetArgs);
-				
-				try {
-					// Call around if registered.  If not, call original
-					result = (around.advice||orig).apply(this, targetArgs);
-					callAdviceWithContext(on, targetArgs);
-
-				} catch(e) {
-					// If an exception was thrown, save it as the result,
-					// and switch to afterThrowing
-					result = e;
-					afterType = afterThrowing;
-
-				}
-
-				// Set args for after* advice types
-				targetArgs = [result];
-
-				// Call the appropriate afterReturning/Throwing advice type based
-				// on the outcome of calling the original func or around advice
-				callAdviceWithContext(afterType, targetArgs);
-
-				// Always call "after", regardless of success return or exception.
-				callAdviceWithContext(after, targetArgs);
-
-				// If the original (or around) threw an exception, rethrow
-				// Otherwise, return the result
-				if(afterType === afterThrowing) {
-					throw result;
-				}
-				
-				return result;
-			};
-
-			advised._advisor = {
-				before:         makeAdviceAdd(before, prepend),
-				on:             makeAdviceAdd(on, append),
-				afterReturning: makeAdviceAdd(afterReturning, append),
-				afterThrowing:  makeAdviceAdd(afterThrowing, append),
-				after:          makeAdviceAdd(after, append),
-				around: function(adviceFunc) {
-					// Allow around "stacking" by wrapping existing around,
-					// if it exists.  If not, wrap orig method.
-					var aroundee = around.advice || orig;
-
-					around.advice = function() {
-						var args, self, proceed, joinpoint;
-						
-						// Proceed to next around or original
-						proceed = function(modifiedArgs) {
-							return aroundee.apply(self, modifiedArgs || args);
-						};
-
-						// Joinpoint representing the original method call
-						joinpoint = {
-							// Original arguments
-							args:   (args = argsToArray(arguments)),
-							// Target object on which the method was called
-							target: (self = this),
-							// The name of the method that was called
-							method: func,
-							// Proceed function.  Advice function should call this to trigger
-							// the next around or the original method invocation
-							proceed: function(modifiedArgs) {
-								// Call next around or original and get result
-								var result = proceed(modifiedArgs);
-
-								// Overwrite proceed to ensure the original can only be called once
-								proceed = function() { throw new Error("proceed() already called"); };
-
-								return result;
-							}
-						};
-
-						// Call outermost around advice to start the chain
-						return adviceFunc.call(self, joinpoint);
-					};
-				}
-			};			
-		}
-		
-		return advised._advisor;
-	}
-
-	// Add a single advice, creating a new advisor for the target func, if necessary.
-	function addAdvice(target, func, type, adviceFunc) {
-		var advisor = getAdvisor(findTarget(target), func);
-
-		if(advisor) {
-			advisor[type](adviceFunc);
-		}
-		
-		return advisor;
-	}
-	
-	// Add several advice types to func
-	function addToFunc(object, funcName, advice) {
-		// advice is an object, and should have keys for advice types,
-		// whose values are the advice functions.
-
-		// First, get the advisor for this object/func pair
-		var advisor, addAdvice;
-		
-		advisor = getAdvisor(object, funcName);
-
-		if(advisor) {
-			// Register all advices with the advisor
-			for (var a in advice) {
-				addAdvice = advisor[a];
-				if (addAdvice) {
-					addAdvice(advice[a]);
-				}
+		return function() {
+			for (var i=removers.length-1; i>=0; i--) {
+				removers[i]();
 			}
 		}
 	}
 
-	function adviseAll(object, funcArray, advice) {
-		var f, i = 0;
-		while((f = funcArray[i++])) {
-			advice(object, f);
-		}
-	}
-
-	function addAspect2(target, pointcut, advice) {
+	function addAspect(target, pointcut, aspect) {
 		// pointcut can be: string, Array of strings, RegExp, Function(targetObject): Array of strings
 		// advice can be: object, Function(targetObject, targetMethodName)
-		
-		var pointcutType, adviceFunc;
 
+		var pointcutType, remove;
 		target = findTarget(target);
 
-		adviceFunc = typeof advice === 'function'
-			? advice
-			: function(object, funcName) {
-				addToFunc(object, funcName, advice);
-			};
-
 		if (isArray(pointcut)) {
-			adviseAll(target, pointcut, adviceFunc);
+			remove = addAspectToAll(target, pointcut, aspect);
 
 		} else {
 			pointcutType = typeof pointcut;
 
 			if (pointcutType === 'string') {
 				if (typeof target[pointcut] === 'function') {
-					adviceFunc(target, pointcut);
+					remove = addAspectToMethod(target, pointcut, aspect);
 				}
 
 			} else if (pointcutType === 'function') {
-				adviseAll(target, pointcut(target), adviceFunc);
+				remove = addAspectToAll(target, pointcut(target), aspect);
 
 			} else {
 				// Assume the pointcut is a RegExp
@@ -376,14 +224,138 @@ define([], function() {
 					// Only apply to own properties that are functions, and match the pointcut regexp
 					if (typeof target[p] === 'function' && pointcut.test(p)) {
 						// if(object.hasOwnProperty(p) && typeof object[p] === 'function' && pointcut.test(p)) {
-						adviceFunc(target, p);
+						remove = addAspectToMethod(target, p, aspect);
 
 					}
 				}
 
 			}
 		}
+
+		return remove;
+
 	}
+	
+	// Returns the advisor for the target object-function pair.  A new advisor
+	// will be created if one does not already exist.
+//	function getAdvisor(target, func) {
+//		if(!(func in target)) return;
+//
+//		var advised = target[func];
+//
+//		if(typeof advised !== 'function') throw new Error('Advice can only be applied to functions: ' + func);
+//
+//		if(!advised._advisor) {
+//			var orig, before, on, around, afterReturning, afterThrowing, after;
+//
+//			// Save the original, not-yet-advised function
+//			orig = advised;
+//
+//			// Advices.  They'll be invoked in this order.
+//			before = [];
+//			around = {};
+//			on = [];
+//			afterReturning = [];
+//			afterThrowing  = [];
+//			after = [];
+//
+//			// Intercept calls to the original function, and invoke
+//			// all currently registered before, around, and after advices
+//			advised = target[func] = function() {
+//				var self, targetArgs, result, afterType;
+//
+//				self = this;
+//				targetArgs = argsToArray(arguments);
+//				afterType = afterReturning;
+//
+//				function callAdviceWithContext(advice, targetArgs) {
+//					callAdvice(advice, self, targetArgs);
+//				}
+//
+//				// Befores
+//				callAdviceWithContext(before, targetArgs);
+//
+//				try {
+//					// Call around if registered.  If not, call original
+//					result = (around.advice||orig).apply(this, targetArgs);
+//					callAdviceWithContext(on, targetArgs);
+//
+//				} catch(e) {
+//					// If an exception was thrown, save it as the result,
+//					// and switch to afterThrowing
+//					result = e;
+//					afterType = afterThrowing;
+//
+//				}
+//
+//				// Set args for after* advice types
+//				targetArgs = [result];
+//
+//				// Call the appropriate afterReturning/Throwing advice type based
+//				// on the outcome of calling the original func or around advice
+//				callAdviceWithContext(afterType, targetArgs);
+//
+//				// Always call "after", regardless of success return or exception.
+//				callAdviceWithContext(after, targetArgs);
+//
+//				// If the original (or around) threw an exception, rethrow
+//				// Otherwise, return the result
+//				if(afterType === afterThrowing) {
+//					throw result;
+//				}
+//
+//				return result;
+//			};
+//
+//			advised._advisor = {
+//				before:         makeAdviceAdd(before, prepend),
+//				on:             makeAdviceAdd(on, append),
+//				afterReturning: makeAdviceAdd(afterReturning, append),
+//				afterThrowing:  makeAdviceAdd(afterThrowing, append),
+//				after:          makeAdviceAdd(after, append),
+//				around: function(adviceFunc) {
+//					// Allow around "stacking" by wrapping existing around,
+//					// if it exists.  If not, wrap orig method.
+//					var aroundee = around.advice || orig;
+//
+//					around.advice = function() {
+//						var args, self, proceed, joinpoint;
+//
+//						// Proceed to next around or original
+//						proceed = function(modifiedArgs) {
+//							return aroundee.apply(self, modifiedArgs || args);
+//						};
+//
+//						// Joinpoint representing the original method call
+//						joinpoint = {
+//							// Original arguments
+//							args:   (args = argsToArray(arguments)),
+//							// Target object on which the method was called
+//							target: (self = this),
+//							// The name of the method that was called
+//							method: func,
+//							// Proceed function.  Advice function should call this to trigger
+//							// the next around or the original method invocation
+//							proceed: function(modifiedArgs) {
+//								// Call next around or original and get result
+//								var result = proceed(modifiedArgs);
+//
+//								// Overwrite proceed to ensure the original can only be called once
+//								proceed = function() { throw new Error("proceed() already called"); };
+//
+//								return result;
+//							}
+//						};
+//
+//						// Call outermost around advice to start the chain
+//						return adviceFunc.call(self, joinpoint);
+//					};
+//				}
+//			};
+//		}
+//
+//		return advised._advisor;
+//	}
 
     function findTarget(target) {
         return target.prototype || target;
@@ -402,9 +374,11 @@ define([], function() {
 	// Public API
 	return {
 		// General add aspect
+		// Returns a function that will remove the newly-added aspect
 		add:            addAspect,
 
 		// Add a single, specific type of advice
+		// returns a function that will remove the newly-added advice
 		before:         adviceApi('before'),
 		around:         adviceApi('around'),
 		on:             adviceApi('on'),
